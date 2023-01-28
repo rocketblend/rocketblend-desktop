f45e7d0500f3a70fa655afce2982cd353f8c631d
Elliot <git@lazercube.com>
2023-01-28 23:00:47 +0000

    remove files

--- frontend/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-export const component = async () => (await import('../entries/pages/_layout.svelte.js')).default;
-export const file = '_app/immutable/components/pages/_layout.svelte-007f8f6c.js';
-export const imports = ["_app/immutable/components/pages/_layout.svelte-007f8f6c.js","_app/immutable/chunks/index-a1c20a72.js"];
-export const stylesheets = ["_app/immutable/assets/_layout-44365102.css"];
-export const fonts = [];
--- frontend/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/components/error.svelte-0bece264.js';
-export const imports = ["_app/immutable/components/error.svelte-0bece264.js","_app/immutable/chunks/index-a1c20a72.js","_app/immutable/chunks/singletons-12c01d42.js"];
-export const stylesheets = [];
-export const fonts = [];
