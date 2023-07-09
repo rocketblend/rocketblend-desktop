52ac060006d6827b4f622998233c236b0eeef452
Elliot <git@lazercube.com>
2023-07-09 19:35:35 +0100

    Upgraded packages

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -12,7 +12,6 @@ const config = {
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
     ...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()
   ],
 };
