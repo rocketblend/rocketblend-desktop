43c4973ae9c5803a795878d7c16844e3a5a97da1
Elliot <git@lazercube.com>
2023-07-09 20:14:52 +0100

    outline project

--- app.go
+++ app.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/rocketblend/rocketblend-desktop/internal/driver"
+
 	"github.com/wailsapp/wails/v2/pkg/menu"
 	"github.com/wailsapp/wails/v2/pkg/menu/keys"
 	"github.com/wailsapp/wails/v2/pkg/runtime"
@@ -11,18 +13,15 @@ import (
 
 // App struct
 type App struct {
-	ctx context.Context
+	driver driver.Driver
+	ctx    context.Context
 }
 
 // NewApp creates a new App application struct
-func NewApp() *App {
-	return &App{}
-}
-
-// startup is called when the app starts. The context is saved
-// so we can call the runtime methods
-func (a *App) startup(ctx context.Context) {
-	a.ctx = ctx
+func New() *App {
+	return &App{
+		driver: driver.New(),
+	}
 }
 
 // Greet returns a greeting for the given name
@@ -30,8 +29,15 @@ func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
 }
 
+// Quit quits the application
 func (a *App) Quit() {
 	runtime.Quit(a.ctx)
+}
+
+// startup is called when the app starts. The context is saved
+// so we can call the runtime methods
+func (a *App) startup(ctx context.Context) {
+	a.ctx = ctx
 }
 
 // menu returns the application menu
--- main.go
+++ main.go
@@ -21,7 +21,7 @@ var assets embed.FS
 
 func main() {
 	// Create an instance of the app structure
-	app := NewApp()
+	app := New()
 
 	// Create application with options
 	err := wails.Run(&options.App{
