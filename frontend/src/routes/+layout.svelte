d0267dae977cec2cd973a3f42acef1e50779580e
Elliot <git@lazercube.com>
2023-07-11 17:00:14 +0100

    More changes

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -5,36 +5,36 @@
 
   import { AppBar, AppShell } from '@skeletonlabs/skeleton';
 
-  import IconCloseRounded from '~icons/material-symbols/close-rounded'
-  import IconSelectWindowSharp from '~icons/material-symbols/select-window-sharp'
-  import IconRemove from '~icons/material-symbols/remove'
-  import IconMoreHoriz from '~icons/material-symbols/more-horiz'
+  import IconCloseFill from '~icons/ri/close-fill'
+  import IconMoreFill from '~icons/ri/more-fill'
+  import IconSubtractFill from '~icons/ri/subtract-fill'
+  import IconCheckboxMultipleBlankLine from '~icons/ri/checkbox-multiple-blank-line'
 </script>
 
 <AppShell>
   <svelte:fragment slot="header">
     <div style="--wails-draggable:drag">
-      <AppBar background="bg-surface-100-900-token" padding="p0" slotTrail="space-x-0">
+      <AppBar background="bg-surface-100-900-token" padding="p0" slotTrail="space-x-0 -mt-3">
           <svelte:fragment slot="lead">
-            <button type="button" class="btn btn-sm py-2 px-4 rounded-none">
-              <IconMoreHoriz/>
+            <button type="button" class="btn btn-sm py-2 px-4 rounded-none text-2xl">
+              <IconMoreFill/>
             </button>
           </svelte:fragment>
           <svelte:fragment slot="trail">
             <button type="button" class="btn btn-sm py-2 px-4 hover:bg-stone-700 rounded-none">
-              <IconRemove/>
+              <IconSubtractFill/>
             </button>
             <button type="button" class="btn btn-sm py-2 px-4 hover:bg-stone-700 rounded-none">
-              <IconSelectWindowSharp/>
+              <IconCheckboxMultipleBlankLine/>
             </button>
             <button type="button" class="btn btn-sm py-2 px-4 hover:bg-red-700 rounded-none">
-              <IconCloseRounded/>
+              <IconCloseFill/>
             </button>
           </svelte:fragment>
       </AppBar>
     </div>
   </svelte:fragment>
-  <div class="p-2 h-full">
+  <div class="h-full p-2 pt-0">
     <div class="shadow-none card p-4 h-full">
       <slot />
     </div>
