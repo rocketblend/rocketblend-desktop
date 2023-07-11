e986a28c1c39b283851caa178f1fd161fc56ad62
Elliot <git@lazercube.com>
2023-07-11 16:38:18 +0100

    Colour

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -11,16 +11,32 @@
   import IconMoreHoriz from '~icons/material-symbols/more-horiz'
 </script>
 
-<AppShell regionPage="p-2">
+<AppShell>
   <svelte:fragment slot="header">
     <div style="--wails-draggable:drag">
-      <AppBar background="bg-surface-100-900-token" padding="px-4 pt-2 pb-0" slotTrail="space-x-7">
-          <svelte:fragment slot="lead"><IconMoreHoriz class="text-2xl"/></svelte:fragment>
-          <svelte:fragment slot="trail"><IconRemove/><IconSelectWindowSharp/><IconCloseRounded/></svelte:fragment>
+      <AppBar background="bg-surface-100-900-token" padding="p0" slotTrail="space-x-0">
+          <svelte:fragment slot="lead">
+            <button type="button" class="btn btn-sm py-2 px-4 rounded-none">
+              <IconMoreHoriz/>
+            </button>
+          </svelte:fragment>
+          <svelte:fragment slot="trail">
+            <button type="button" class="btn btn-sm py-2 px-4 hover:bg-stone-700 rounded-none">
+              <IconRemove/>
+            </button>
+            <button type="button" class="btn btn-sm py-2 px-4 hover:bg-stone-700 rounded-none">
+              <IconSelectWindowSharp/>
+            </button>
+            <button type="button" class="btn btn-sm py-2 px-4 hover:bg-red-700 rounded-none">
+              <IconCloseRounded/>
+            </button>
+          </svelte:fragment>
       </AppBar>
     </div>
   </svelte:fragment>
-  <div class="shadow-none card p-4 h-full">
-    <slot />
+  <div class="p-2 h-full">
+    <div class="shadow-none card p-4 h-full">
+      <slot />
+    </div>
   </div>
 </AppShell>
