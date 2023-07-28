d782b3f8b7cc4e5f4ea1fa965e8b626d86b08135
Elliot <git@lazercube.com>
2023-07-28 20:50:49 +0100

    Added type toggle

--- frontend/src/lib/components/project-list/project-list.svelte
+++ frontend/src/lib/components/project-list/project-list.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import { onMount } from 'svelte';
+
+    import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
     
 	import ProjectGallery from '../project-gallery/project-gallery.svelte';
     import ProjectTable from '../project-table/project-table.svelte';
@@ -10,6 +12,7 @@
 
     let projects: project.Project[] = [];
     let searchTerm: string = '';
+    let displayType: number = 0;
 
     async function loadProjects(query: string): Promise<void> {
         const result = await FindAllProjects(query);
@@ -20,20 +23,34 @@
         searchTerm = event.detail;
     }
 
-    // Load initial project list
     onMount(async () => {
         await loadProjects(searchTerm);
     });
 
-    // Reactive statement to fetch new project list when searchTerm changes
     $: loadProjects(searchTerm), searchTerm;
 </script>
 
 <div class="space-y-4">
-    <div>
-        <SearchInput on:search={handleSearch} placeholder="Search"/>
+    <div class="flex items-center justify-between space-x-4">
+        <div class="w-full">
+            <SearchInput on:search={handleSearch} placeholder="Search"/>
+        </div>
+        <RadioGroup>
+            <RadioItem bind:group={displayType} name="justify" value={0}>Table</RadioItem>
+            <RadioItem bind:group={displayType} name="justify" value={1}>Gallery</RadioItem>
+        </RadioGroup>
     </div>
     
-    <ProjectTable sourceData={projects}/>
-    <!-- <ProjectGallery/> -->
+    {#if projects.length === 0}
+        <div class="flex items-center justify-center h-64">
+            <h4>No projects found.</h4>
+        </div>
+    {:else}
+        {#if displayType === 0}
+            <ProjectTable sourceData={projects}/>
+        {:else if displayType === 1}
+            <ProjectGallery />
+        {/if}
+    {/if}
+
 </div>
