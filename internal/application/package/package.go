64ceb8cbafcb2a0f66ec4d52b0198b4917efc157
Elliot <git@lazercube.com>
2023-11-27 16:20:26 +0000

    Changes

--- internal/application/package/package.go
+++ internal/application/package/package.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/rocketblend/rocketblend-desktop/internal/application/util"
-	"github.com/rocketblend/rocketblend/pkg/downloader"
 	"github.com/rocketblend/rocketblend/pkg/driver/reference"
 	"github.com/rocketblend/rocketblend/pkg/driver/rocketpack"
 	"github.com/rocketblend/rocketblend/pkg/driver/runtime"
@@ -19,65 +18,49 @@ const packageFolderName = "packages" // TODO: This isn't currently guaranteed to
 
 type (
 	Package struct {
-		ID           uuid.UUID             `json:"id,omitempty"`
-		Type         string                `json:"type,omitempty"`
-		Reference    reference.Reference   `json:"reference,omitempty"`
-		Name         string                `json:"name,omitempty"`
-		Path         string                `json:"path,omitempty"`
-		Version      *semver.Version       `json:"version,omitempty"`
-		Dependencies []reference.Reference `json:"addons,omitempty"`
-		Resource     string                `json:"resource,omitempty"`
-		URI          *downloader.URI       `json:"uri,omitempty"`
-		UpdatedAt    time.Time             `json:"updatedAt,omitempty"`
+		ID               uuid.UUID             `json:"id,omitempty"`
+		Type             Type                  `json:"type,omitempty"`
+		Reference        reference.Reference   `json:"reference,omitempty"`
+		Name             string                `json:"name,omitempty"`
+		Path             string                `json:"path,omitempty"`
+		InstallationPath string                `json:"installationPath,omitempty"`
+		Version          semver.Version        `json:"version,omitempty"`
+		Dependencies     []reference.Reference `json:"addons,omitempty"`
+		Sources          rocketpack.Sources
+		UpdatedAt        time.Time `json:"updatedAt,omitempty"`
 	}
 )
 
-func Load(packagePath string, platform runtime.Platform) (*Build, *Addon, error) {
+func Load(packagePath string, platform runtime.Platform) (*Package, error) {
 	pack, err := rocketpack.Load(packagePath)
 	if err != nil {
-		return nil, nil, err
-	}
-
-	if pack.IsAddon() {
-		addon, err := NewAddon(packagePath, pack)
-		return nil, addon, err
-	}
-
-	build, err := NewBuild(packagePath, platform, pack)
-	return build, nil, err
-}
-
-func LoadPack(packagePath string, platform runtime.Platform) (*Package, error) {
-	pack, err := rocketpack.Load(packagePath)
-	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error loading package: %w", err)
 	}
 
 	reference, err := pathToReference(packagePath)
 	if err != nil {
-		return nil, err
-	}
-
-	name, err := reference.GetRepo()
-	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error getting path reference for package: %w", err)
 	}
 
 	modTime, err := util.GetDirModTime(packagePath)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error getting package modification time: %w", err)
 	}
 
+	packType := Addon
+	name := pack.Addon.Name
 	version := pack.Addon.Version
-	resource := pack.Addon.Source.Resource
-	uri := pack.Addon.Source.URI
-	packType := "addon"
+	sources := make(rocketpack.Sources)
+	sources[runtime.Undefined] = &rocketpack.Source{
+		Resource: pack.Addon.Source.Resource,
+		URI:      pack.Addon.Source.URI,
+	}
 
 	if pack.IsBuild() {
+		packType = Build
 		version = pack.Build.Version
-		resource = pack.Build.Sources[platform].Resource
-		uri = pack.Build.Sources[platform].URI
-		packType = "build"
+		sources = pack.Build.Sources
+		name = reference.String()
 	}
 
 	return &Package{
@@ -86,10 +69,9 @@ func LoadPack(packagePath string, platform runtime.Platform) (*Package, error) {
 		Name:         name,
 		Reference:    reference,
 		Path:         packagePath,
+		Sources:      sources,
 		Dependencies: pack.GetDependencies(),
-		Version:      version,
-		Resource:     resource,
-		URI:          uri,
+		Version:      *version,
 		UpdatedAt:    modTime,
 	}, nil
 }
