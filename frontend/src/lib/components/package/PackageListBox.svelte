6c477e1dc43cd68ea7b8c411482a7d4a82a5e583
Elliot <git@lazercube.com>
2023-12-10 15:35:04 +0000

    Package Item

--- frontend/src/lib/components/package/PackageListBox.svelte
+++ frontend/src/lib/components/package/PackageListBox.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
     import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
+    import PackageItem from './PackageItem.svelte';
     import type { pack } from '$lib/wailsjs/go/models';
-  
+
     export let packages: pack.Package[] = [];
     export let selectedPackageIds: string[] = [];
 </script>
@@ -9,20 +10,7 @@
 <ListBox class="flex-auto" multiple>
     {#each packages as pack}
         <ListBoxItem bind:group={selectedPackageIds} name="packages" value={pack.id} active="variant-glass-primary" hover="hover:variant-filled-surface" rounded="rounded" class="truncate overflow-hidden">
-            <div class="flex gap-2">
-                <!-- <div class="flex-shrink-0 inline-flex items-center">
-                    {#if pack.type == 'build'}
-                        <IconCheckboxBlankCircleLine />
-                    {:else}
-                        <IconCheckboxBlankLine />
-                    {/if}
-                </div> -->
-                <div class="flex-grow w-2">
-                    <div class="text-sm font-medium">{pack.name}</div>
-                    <div class="text-sm">{pack.reference}</div>
-                </div>
-            </div>
+            <PackageItem name={pack.name || ""} reference={pack.reference || ""} />
         </ListBoxItem>
     {/each}
 </ListBox>
-
