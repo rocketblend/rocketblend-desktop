1785cd7003f74b90ab7b12ccba45d8b68043e7f7
Elliot <git@lazercube.com>
2023-12-12 20:05:34 +0000

    Changed startup window size

--- internal/application/app.go
+++ internal/application/app.go
@@ -92,8 +92,8 @@ func (a *application) Execute() error {
 	// Create application with options
 	return wails.Run(&options.App{
 		Title:  "RocketBlend Desktop",
-		Width:  1148,
-		Height: 768,
+		Width:  1150,
+		Height: 780,
 		AssetServer: &assetserver.Options{
 			Assets:  a.assets,
 			Handler: a.handler,
