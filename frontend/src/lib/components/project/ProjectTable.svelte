b97caf542f22eb62dbd72d1f2a476edbfe1cb374
Elliot <git@lazercube.com>
2023-12-20 20:50:54 +0000

    Fixed spaces

--- frontend/src/lib/components/project/ProjectTable.svelte
+++ frontend/src/lib/components/project/ProjectTable.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
-  import type { project } from '$lib/wailsjs/go/models';
-  import { tableMapperValues } from '$lib/components/core'
+    import type { project } from '$lib/wailsjs/go/models';
+    import { tableMapperValues } from '$lib/components/core'
 
-  import Table from '$lib/components/core/table/Table.svelte';
-  import type { TableSource } from '$lib/components/core/table/types.js';
+    import Table from '$lib/components/core/table/Table.svelte';
+    import type { TableSource } from '$lib/components/core/table/types.js';
 
-  export let sourceData: project.Project[];
-  export let selectedProjectIds: string[] = [];
+    export let sourceData: project.Project[];
+    export let selectedProjectIds: string[] = [];
 
-  let tableSource: TableSource;
+    let tableSource: TableSource;
 
-  $: {
-    tableSource = {
-      head: ['Project', 'File', 'Build', 'Tags'],
-      body: tableMapperValues(sourceData, 'id', ['name', 'fileName', 'build', 'tags']),
-    };
-  }
+    $: {
+        tableSource = {
+            head: ['Project', 'File', 'Build', 'Tags'],
+            body: tableMapperValues(sourceData, 'id', ['name', 'fileName', 'build', 'tags']),
+        };
+    }
 </script>
 
 <Table bind:source={tableSource} interactive={true} bind:selected={selectedProjectIds} />
