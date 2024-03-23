a0c4113f3fa92e6b5d02f6375c9db2f0911d6ad9
Elliot <git@lazercube.com>
2024-03-23 23:32:57 +0000

    new preference page outlined design

--- frontend/src/routes/(components)/utility-drawer/debug-tool/index.ts
+++ frontend/src/routes/(components)/utility-drawer/debug-tool/index.ts
@@ -1,3 +1,3 @@
 export { default as DebugToolMetrics } from "./debug-tool-metric.svelte"
-export { default as DebugToolOperations } from "./debug-tool-metric.svelte"
+export { default as DebugToolOperations } from "./debug-tool-operation.svelte"
 export { default as DebugToolRequests } from "./debug-tool-request.svelte"
--- frontend/src/routes/preferences/+page.svelte
+++ frontend/src/routes/preferences/+page.svelte
@@ -2,8 +2,7 @@
     import type { PageData } from "./$types";
 
     import { t } from '$lib/translations/translations';
-
-	import { GeneralForm } from './(components)/forms';
+	import { SlideToggle } from "@skeletonlabs/skeleton";
 
     export let data: PageData;
 </script>
@@ -13,11 +12,63 @@
         <h2 class="h2 font-bold">{$t('preference.title')}</h2>
     </div>
     <hr>
-    <div class="space-y-4">
-        <h3 id="general" class="h3 font-bold">{$t('preference.category.general.title')}</h3>
-        <GeneralForm data={data.form}/>
-    </div>
-    <div class="space-y-2">
-        <h3 id="rocketblend" class="h3 font-bold">{$t('preference.category.rocketblend.title')}</h3>
+    <div class="space-y-8">
+        <div class="space-y-3">
+            <h5 class="h6 font-bold">General</h5>
+            <div class="flex justify-between items-center gap-6">
+                <div class="text-sm text-left">
+                    <span class="font-medium">Project watch directory</span><br>
+                    <span class="text-surface-200 ">C:\Users\user\Documents\Projects</span>
+                </div>
+                <button class="btn variant-filled-surface text-sm font-medium">
+                    Change location
+                </button>
+            </div>
+
+            <div class="flex justify-between items-center gap-6">
+                <div class="text-sm text-left">
+                    <span class="font-medium">Configuration file</span><br>
+                    <span class="text-surface-200 ">C:\Users\user\appdata\roaming\rocketblend-desktop\settings.json</span>
+                </div>
+                <button class="btn variant-filled-surface text-sm font-medium">
+                    View location
+                </button>
+            </div>
+        </div>
+        
+        <div class="space-y-3">
+            <h5 class="h6 font-bold">RocketBlend</h5>
+            <div class="flex justify-between items-center gap-6">
+                <div class="text-sm text-left">
+                    <span class="font-medium">Configuration file</span><br>
+                    <span class="text-surface-200 ">C:\Users\user\appdata\roaming\rocketblend\settings.json</span>
+                </div>
+                <button class="btn variant-filled-surface text-sm font-medium">
+                    View location
+                </button>
+            </div>
+        </div>
+
+        <div class="space-y-3">
+            <div>
+                <h5 class="h6 font-bold">Experimental</h5>
+                <p class="text-sm text-surface-200">
+                    Enable experimental features to try out new functionalities before they're fully released.
+                    <span class="font-medium">Please note, these features might be unstable or incomplete.</span>
+                </p>
+            </div>
+            <div class="flex justify-between items-center gap-6">
+                <div class="text-sm text-left">
+                    Addons - <span class="text-surface-200">Configure and install blender addons for projects.</span>
+                </div>
+                <SlideToggle name="" size="sm" active="bg-secondary-500"/>
+            </div>
+            <div class="flex justify-between items-center gap-6">
+                <div class="text-sm text-left">
+                    Developer mode - <span class="text-surface-200">Access advanced settings and features for development purposes.</span>
+                </div>
+                <SlideToggle name="" size="sm" active="bg-secondary-500"/>
+            </div>
+        </div>
     </div>
 </main>
