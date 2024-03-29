db71b34fcac96678516a6a6c4d024847327d361f
Elliot <git@lazercube.com>
2024-03-29 17:13:34 +0000

    added hr

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,8 +3,12 @@
 </script>
 
 <main class="flex flex-col h-full space-y-8">
-    <h2 class="h2 font-bold">Welcome!</h2>
-    <div class="flex flex-col gap-2">
+    <div class="flex flex-col space-y-4">
+        <h2 class="h2 font-bold">Welcome!</h2>
+        <hr>
+    </div>
+
+    <!-- <div class="flex flex-col gap-2">
         <h3 class="text-sm text-surface-800-100-token">Recent projects</h3>
         <div class="grid gap-2 grid-cols-2 lg:grid-cols-3">
             <a class="flex items-center justify-center card p-4" href="/projects">
@@ -13,7 +17,7 @@
                 </div>
             </a>
         </div>
-    </div>
+    </div> -->
     <div class="flex flex-col gap-2">
         <h3 class="text-sm text-surface-800-100-token">Shortcuts</h3>
         <div class="grid gap-2 grid-cols-2 lg:grid-cols-4">
--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -2,6 +2,7 @@
     import { t } from '$lib/translations/translations';
 </script>
 
-<main class="flex flex-col h-full space-y-8">
+<main class="flex flex-col h-full space-y-4">
     <h2 class="h2 font-bold">About</h2>
+    <hr>
 </main>
