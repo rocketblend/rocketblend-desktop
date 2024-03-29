db0575bf11c98d3f76d2b814f73339b7c4b75608
Elliot <git@lazercube.com>
2024-03-29 17:44:08 +0000

    fixed issue with labels

--- frontend/src/routes/(components)/breadcrumb/breadcrumb.svelte
+++ frontend/src/routes/(components)/breadcrumb/breadcrumb.svelte
@@ -4,21 +4,22 @@
     let crumbs: Array<{ label: string, href: string }> = [];
 
     $: {
-        // Remove zero-length tokens.
         const tokens = $page.url.pathname.split('/').filter((t) => t !== '');
 
-        // Create { label, href } pairs for each token.
         let tokenPath = '';
-        crumbs = tokens.map((t) => {
-        tokenPath += '/' + t;
-        t = t.charAt(0).toUpperCase() + t.slice(1);
+        crumbs = tokens.map((t, index) => {
+            tokenPath += '/' + t;
+
+            const formattedToken = t.charAt(0).toUpperCase() + t.slice(1);
+            const isCurrentPage = index === tokens.length - 1;
+            const label = isCurrentPage && $page.data.label ? $page.data.label : formattedToken;
+
             return {
-                label: $page.data.label || t,
+                label: label,
                 href: tokenPath
             };
         });
 
-        // Add a way to get home too.
         crumbs.unshift({ label: 'Home', href: '/' });
     }
 </script>
@@ -26,7 +27,6 @@
 {#if $page.data.showBreadcrumb}
     <ol class="breadcrumb text-sm text-surface-800-100-token truncate">
         {#each crumbs as crumb, i}
-            <!-- If crumb index is less than the breadcrumb length minus 1 -->
             {#if i < crumbs.length - 1}
                 <li class="crumb"><a href={crumb.href}>{crumb.label}</a></li>
                 <li class="crumb-separator" aria-hidden>&rsaquo;</li>
