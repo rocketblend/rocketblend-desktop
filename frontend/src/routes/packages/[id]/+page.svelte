e482f38b13808c3a68bbfc85bb438d7c5cf4da1d
Elliot <git@lazercube.com>
2024-03-30 15:22:37 +0000

    added basics for package pages

---
+++ frontend/src/routes/packages/[id]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+
+    import { t } from '$lib/translations/translations';
+
+    export let data: PageData;
+</script>
+
+<main class="flex flex-col h-full space-y-4">
+    <h2 class="h2 font-bold">{data.package.name}</h2>
+    <hr>
+</main>
---
+++ frontend/src/routes/packages/[id]/+page.ts
@@ -0,0 +1,18 @@
+import type { PageLoad } from './$types';
+import { error} from '@sveltejs/kit';
+import { GetPackage } from '$lib/wailsjs/go/application/Driver'
+
+export const load: PageLoad = async ({ params }) => {
+    const result = await GetPackage(params.id);
+
+    if (!result || result.package === undefined) {
+        throw error(404, {
+            message: 'Package not found'
+        });
+    }
+
+    return {
+        label: result.package.name,
+        package: result.package
+    };
+};
