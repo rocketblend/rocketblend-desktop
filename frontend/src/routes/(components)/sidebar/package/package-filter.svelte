6cbb333eb79f5a4fa3b63c32aac93316e21bb15a
Elliot <git@lazercube.com>
2024-03-30 14:29:11 +0000

    hide addons if flag is off

--- frontend/src/routes/(components)/sidebar/package/package-filter.svelte
+++ frontend/src/routes/(components)/sidebar/package/package-filter.svelte
@@ -8,7 +8,7 @@
     export let selectedFilterType: number;
     export let searchQuery: string;
     export let filterInstalled: boolean;
-    export let filterRadioOptions: RadioOption[];
+    export let filterRadioOptions: RadioOption[] = [];
     export let searchPlaceholder: string;
     export let installedLabel: string;
 
@@ -20,13 +20,15 @@
 </script>
 
 <div class="flex flex-col space-y-4">
-    <RadioGroup display="inline-flex">
-        {#each filterRadioOptions as option}
-            <RadioItem bind:group={selectedFilterType} name="justify" value={option.value} class="text-sm" on:change={handleFilterChange}>
-                {option.display}
-            </RadioItem>
-        {/each}
-    </RadioGroup>
+    {#if filterRadioOptions.length > 0}
+        <RadioGroup display="inline-flex">
+            {#each filterRadioOptions as option}
+                <RadioItem bind:group={selectedFilterType} name="justify" value={option.value} class="text-sm" on:change={handleFilterChange}>
+                    {option.display}
+                </RadioItem>
+            {/each}
+        </RadioGroup>
+    {/if}
     <InputSearch bind:value={searchQuery} placeholder={searchPlaceholder} debounceDelay={500} on:input={handleFilterChange} class="text-sm"/>
     <SlideToggle name="slider-label" size="sm" active="bg-surface-200" class="text-sm" border="ring-outline-token" bind:checked={filterInstalled} on:change={handleFilterChange}>
         {installedLabel}
