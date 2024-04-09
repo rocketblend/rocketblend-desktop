0590e211ec4ce36884b9a43bd9227354b82c7480
Elliot <git@lazercube.com>
2024-04-09 13:10:19 +0100

    new alert component

---
+++ frontend/src/lib/components/ui/alert/alert-action.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let text: string;
+    export let weight: "medium" | "bold" = "medium";
+    export let variant: "filled-surface" | "glass-surface" = "filled-surface";
+</script>
+
+<button class="btn btn-sm font-{weight} variant-{variant}" on:click>
+    {text}
+</button>
--- frontend/src/routes/packages/[id]/(components)/package-actions.svelte
+++ frontend/src/routes/packages/[id]/(components)/package-actions.svelte
@@ -6,6 +6,12 @@
 
     import { ProgressRadial } from '@skeletonlabs/skeleton';
 
+    import {
+        Alert,
+        AlertTitle,
+        AlertDescription,
+        AlertAction
+    } from '$lib/components/ui/alert';
     import { pack } from '$lib/wailsjs/go/models';
 
     export let state: pack.PackageState = pack.PackageState.AVAILABLE;
@@ -17,60 +23,70 @@
 </script>
 
 {#if state === pack.PackageState.AVAILABLE}
-    <aside class="alert variant-ghost-surface">
-        <IconDownloadFill class="text-2xl"/>
-        <div class="alert-message ">
-            <h2 class="font-bold h6">Available</h2>
-            <p class="text-sm">Package is available to be downloaded.</p>
-        </div>
-        <div class="alert-actions">
-            <button class="btn btn-sm variant-filled-surface font-medium" on:click={cycleState}>Download</button>
-        </div>
-    </aside>
+    <Alert>
+        <svelte:fragment slot="icon">
+            <IconDownloadFill class="text-2xl"/>
+        </svelte:fragment>
+        <svelte:fragment slot="title">
+            <AlertTitle title="Available"/>
+        </svelte:fragment>
+        <AlertDescription message="Package is available to be downloaded."/>
+        <svelte:fragment slot="actions">
+            <AlertAction text="Download" on:click={cycleState}/>
+        </svelte:fragment>
+    </Alert>
 {:else if state === pack.PackageState.DOWNLOADING}
-    <aside class="alert variant-ghost-surface">
-        <ProgressRadial width="w-6" stroke={50} strokeLinecap="square"/>
-        <div class="alert-message ">
-            <h2 class="font-bold h6">Downloading</h2>
-            <p class="text-sm">Package is currently downloading.</p>
-        </div>
-        <div class="alert-actions">
-            <button class="btn btn-sm variant-filled-surface font-medium" on:click={cycleState}>Pause</button>
-        </div>
-    </aside>
+    <Alert>
+        <svelte:fragment slot="icon">
+            <ProgressRadial width="w-6" stroke={50} strokeLinecap="square"/>
+        </svelte:fragment>
+        <svelte:fragment slot="title">
+            <AlertTitle title="Downloading"/>
+        </svelte:fragment>
+        <AlertDescription message="Package is currently downloading."/>
+        <svelte:fragment slot="actions">
+            <AlertAction text="Pause" on:click={cycleState}/>
+        </svelte:fragment>
+    </Alert>
 {:else if state === pack.PackageState.CANCELLED}
-    <aside class="alert variant-ghost-surface">
-        <IconPauseFill class="text-2xl"/>
-        <div class="alert-message ">
-            <h2 class="font-bold h6">Paused</h2>
-            <p class="text-sm">Download of package has been paused.</p>
-        </div>
-        <div class="alert-actions">
-            <button class="btn btn-sm variant-filled-surface font-medium">Cancel</button>
-            <button class="btn btn-sm variant-filled-surface font-medium" on:click={cycleState}>Resume</button>
-        </div>
-    </aside>
+    <Alert>
+        <svelte:fragment slot="icon">
+            <IconPauseFill class="text-2xl"/>
+        </svelte:fragment>
+        <svelte:fragment slot="title">
+            <AlertTitle title="Paused"/>
+        </svelte:fragment>
+        <AlertDescription message="Download of package has been paused."/>
+        <svelte:fragment slot="actions">
+            <AlertAction text="Cancel"/>
+            <AlertAction text="Resume" on:click={cycleState}/>
+        </svelte:fragment>
+    </Alert>
 {:else if state === pack.PackageState.INSTALLED}
-    <aside class="alert variant-ghost-surface">
-        <IconCheckFill class="text-2xl"/>
-        <div class="alert-message ">
-            <h2 class="font-bold h6">Ready</h2>
-            <p class="text-sm">Package is ready to be used.</p>
-        </div>
-        <div class="alert-actions">
-            <button class="btn btn-sm variant-filled-surface font-medium"  on:click={cycleState}>Delete</button>
-            <button class="btn btn-sm variant-filled-surface font-medium">View Location</button>
-        </div>
-    </aside>
+    <Alert>
+        <svelte:fragment slot="icon">
+            <IconCheckFill class="text-2xl"/>
+        </svelte:fragment>
+        <svelte:fragment slot="title">
+            <AlertTitle title="Ready"/>
+        </svelte:fragment>
+        <AlertDescription message="Package is ready to be used."/>
+        <svelte:fragment slot="actions">
+            <AlertAction text="Delete" on:click={cycleState}/>
+            <AlertAction text="View Location"/>
+        </svelte:fragment>
+    </Alert>
 {:else}
-    <aside class="alert variant-ghost-surface">
-        <IconWarningFill class="text-2xl"/>
-        <div class="alert-message ">
-            <h2 class="font-bold h6">Error</h2>
-            <p class="text-sm">An error occurred while downloading the package.</p>
-        </div>
-        <div class="alert-actions">
-            <button class="btn btn-sm variant-filled-surface font-medium" on:click={cycleState}>Retry</button>
-        </div>
-    </aside>
+    <Alert>
+        <svelte:fragment slot="icon">
+            <IconWarningFill class="text-2xl"/>
+        </svelte:fragment>
+        <svelte:fragment slot="title">
+            <AlertTitle title="Error"/>
+        </svelte:fragment>
+        <AlertDescription message="An error occurred while downloading the package."/>
+        <svelte:fragment slot="actions">
+            <AlertAction text="Retry" on:click={cycleState}/>
+        </svelte:fragment>
+    </Alert>
 {/if}
