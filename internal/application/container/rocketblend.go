0b909b5d836b275938f112500a6b4938884024b1
Elliot <git@lazercube.com>
2024-06-10 13:51:11 +0100

    open with macos support

--- internal/application/container/rocketblend.go
+++ internal/application/container/rocketblend.go
@@ -12,3 +12,21 @@ func (c *Container) GetRBConfigurator() (rbtypes.Configurator, error) {
 
 	return configurator, nil
 }
+
+func (c *Container) GetRBDriver() (rbtypes.Driver, error) {
+	driver, err := c.rbContainer.GetDriver()
+	if err != nil {
+		return nil, err
+	}
+
+	return driver, nil
+}
+
+func (c *Container) GetBlender() (rbtypes.Blender, error) {
+	blender, err := c.rbContainer.GetBlender()
+	if err != nil {
+		return nil, err
+	}
+
+	return blender, nil
+}
--- main.go
+++ main.go
@@ -5,11 +5,8 @@ import (
 	"embed"
 	"fmt"
 	"os"
-	"path/filepath"
 
 	"github.com/rocketblend/rocketblend-desktop/internal/application"
-	"github.com/rocketblend/rocketblend/pkg/container"
-	"github.com/rocketblend/rocketblend/pkg/types"
 )
 
 // 'wails dev' should properly launch vite to serve the site
@@ -27,26 +24,11 @@ var version = "dev"
 
 func main() {
 	if err := run(os.Args); err != nil {
-		fmt.Println("Error:", err)
+		fmt.Println("Something went wrong:", err)
 	}
 }
 
 func run(args []string) error {
-	if len(os.Args) > 1 {
-		development := false
-		if version == "dev" {
-			development = true
-		}
-
-		var err error
-		if err = open(context.Background(), os.Args[1], development); err == nil {
-			// If we successfully launched a project, we're done.
-			return nil
-		}
-
-		// If we failed to launch a project directly, open with application.
-	}
-
 	app, err := application.New(application.ApplicationOpts{
 		Assets:  assets,
 		Version: version,
@@ -56,53 +38,16 @@ func run(args []string) error {
 		return err
 	}
 
+	if len(os.Args) > 1 {
+		if err := app.Open(context.Background(), os.Args[1]); err == nil {
+			// If we successfully launched a project, we're done.
+			return nil
+		}
+
+		// If we failed to launch a project directly, open with application.
+	}
+
 	if err := app.Execute(); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func open(ctx context.Context, blendFilePath string, development bool) error {
-	container, err := container.New(
-		container.WithDevelopmentMode(development),
-	)
-	if err != nil {
-		return fmt.Errorf("failed to create container: %w", err)
-	}
-
-	driver, err := container.GetDriver()
-	if err != nil {
-		return err
-	}
-
-	profiles, err := driver.LoadProfiles(ctx, &types.LoadProfilesOpts{
-		Paths: []string{filepath.Dir(blendFilePath)},
-	})
-	if err != nil {
-		return err
-	}
-
-	resolve, err := driver.ResolveProfiles(ctx, &types.ResolveProfilesOpts{
-		Profiles: profiles.Profiles,
-	})
-	if err != nil {
-		return err
-	}
-
-	blender, err := container.GetBlender()
-	if err != nil {
-		return err
-	}
-
-	if err := blender.Run(ctx, &types.RunOpts{
-		BlenderOpts: types.BlenderOpts{
-			BlendFile: &types.BlendFile{
-				Path:         blendFilePath,
-				Dependencies: resolve.Installations[0],
-			},
-		},
-	}); err != nil {
 		return err
 	}
 
