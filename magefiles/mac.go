de940310e90712c291f8e72b91c75cb4b03c3441
Elliot <git@lazercube.com>
2024-10-03 21:08:50 +0100

    removing gon

--- magefiles/mac.go
+++ magefiles/mac.go
@@ -8,20 +8,30 @@ import (
 	"github.com/magefile/mage/sh"
 )
 
-// buildMacOSApp builds the macOS universal version of the project.
+// buildMacOSApp builds the macOS universal version of the project and handles signing and notarization.
 func buildMacOSApp(ldFlags, appName, appVersion string, releaseBuild bool) error {
 	if err := buildMacOSWailsApp(ldFlags); err != nil {
 		return fmt.Errorf("error building Wails app for macOS: %v", err)
 	}
 
 	if releaseBuild {
-		if err := importAndSignMacOSCode(); err != nil {
+		appPath := fmt.Sprintf("./build/bin/%s.app", appName)
+
+		if err := importMacOSCertificate(); err != nil {
+			return fmt.Errorf("error importing certificate: %v", err)
+		}
+
+		if err := signMacOSFile(appPath, appName); err != nil {
+			return fmt.Errorf("error signing .app: %v", err)
+		}
+
+		dmgOutputPath, err := createMacOSDMG(appPath, appName, appVersion)
+		if err != nil {
 			return err
 		}
 
-		dmgOutputPath, err := buildAndSignMacOSDMG(appVersion, appName)
-		if err != nil {
-			return err
+		if err := signMacOSFile(dmgOutputPath, appName); err != nil {
+			return fmt.Errorf("error signing DMG: %v", err)
 		}
 
 		if err := notarizeMacOSDMG(dmgOutputPath); err != nil {
@@ -44,8 +54,8 @@ func buildMacOSWailsApp(ldFlags string) error {
 	return sh.RunV("wails", "build", "-m", "-nosyncgomod", "-ldflags", ldFlags, "-platform", "darwin/universal")
 }
 
-// importAndSignMacOSCode handles code signing and certificate import for macOS.
-func importAndSignMacOSCode() error {
+// importMacOSCertificate imports the certificate into the keychain for code signing.
+func importMacOSCertificate() error {
 	cert := os.Getenv("AC_CERTIFICATE")
 	certPassword := os.Getenv("AC_CERTIFICATE_PASSWORD")
 
@@ -53,57 +63,79 @@ func importAndSignMacOSCode() error {
 		return fmt.Errorf("missing required environment variables: AC_CERTIFICATE or AC_CERTIFICATE_PASSWORD")
 	}
 
-	if err := importMacOSCodeSigningCertificates(cert, certPassword); err != nil {
-		return fmt.Errorf("error importing code signing certificates: %v", err)
+	certBytes, err := base64.StdEncoding.DecodeString(cert)
+	if err != nil {
+		return fmt.Errorf("error decoding base64 certificate: %v", err)
 	}
 
-	fmt.Println("Signing macOS Build")
-	if err := sh.RunV("gon", "-log-level=info", "./build/darwin/gon-sign.json"); err != nil {
-		return fmt.Errorf("error signing macOS build: %v", err)
+	certFilePath := "./cert.p12"
+	if err := os.WriteFile(certFilePath, certBytes, 0600); err != nil {
+		return fmt.Errorf("error writing certificate to file: %v", err)
+	}
+	defer os.Remove(certFilePath)
+
+	if err := sh.Run("security", "import", certFilePath, "-P", certPassword, "-T", "/usr/bin/codesign"); err != nil {
+		return fmt.Errorf("error importing certificate: %v", err)
 	}
 
-	return nil
+	return sh.Run("security", "set-key-partition-list", "-S", "apple-tool:,apple:", "-s", "-k", certPassword, certFilePath)
 }
 
-// buildAndSignMacOSDMG creates and signs the DMG for macOS, returning its output path.
-func buildAndSignMacOSDMG(appName, appVersion string) (string, error) {
+// signMacOSFile signs any file (app, DMG, etc.) with the Developer ID Application certificate.
+func signMacOSFile(filePath, appName string) error {
+	developerID := os.Getenv("AC_DEVELOPER_ID")
+	bundleID := fmt.Sprintf("io.rocketblend.%s", appName)
+
+	if developerID == "" {
+		return fmt.Errorf("missing required environment variable: AC_DEVELOPER_ID")
+	}
+
+	fmt.Printf("Signing file: %s\n", filePath)
+	return sh.RunV("codesign", "--deep", "--force", "--options", "runtime", "--sign", developerID, "--timestamp", "--entitlements", "./entitlements.plist", "--identifier", bundleID, filePath)
+}
+
+// createMacOSDMG creates a DMG from the given .app bundle and sets the icon.
+func createMacOSDMG(appPath, appName, appVersion string) (string, error) {
 	dmgOutputPath := fmt.Sprintf("./build/bin/%s-darwin-universal-%s.dmg", appName, appVersion)
 
 	fmt.Println("Building DMG for macOS")
-	if err := sh.RunV("create-dmg", "--window-size", "800", "300", "--no-internet-enable", "--hide-extension", fmt.Sprintf("%s.app", appName), "--app-drop-link", "600", "40", dmgOutputPath, fmt.Sprintf("./build/bin/%s.app", appName)); err != nil {
+	if err := sh.RunV("create-dmg", "--window-size", "800", "300", "--no-internet-enable", "--hide-extension", "--app-drop-link", "600", "40", dmgOutputPath, appPath); err != nil {
 		return "", fmt.Errorf("error building DMG for macOS: %v", err)
 	}
 
-	developerID := os.Getenv("AC_DEVELOPER_ID")
-	if developerID == "" {
-		return "", fmt.Errorf("missing required environment variable: AC_DEVELOPER_ID")
+	// Compile the Swift script for setting the DMG icon
+	if err := compileSetIconSwiftScript(); err != nil {
+		return "", fmt.Errorf("error compiling seticon Swift script: %v", err)
 	}
 
-	fmt.Println("Signing macOS DMG")
-	if err := sh.RunV("codesign", "--sign", developerID, dmgOutputPath); err != nil {
-		return "", fmt.Errorf("error signing macOS DMG: %v", err)
-	}
-
-	if err := compileAndSetMacOSDMGIcons(dmgOutputPath, appName); err != nil {
-		return "", err
+	// Set the DMG icon using the compiled script
+	iconFilePath := fmt.Sprintf("./build/bin/%s.app/Contents/Resources/iconfile.icns", appName)
+	if err := setDMGIcons(iconFilePath, dmgOutputPath); err != nil {
+		return "", fmt.Errorf("error setting DMG icons: %v", err)
 	}
 
 	return dmgOutputPath, nil
 }
 
-// compileAndSetMacOSDMGIcons compiles and sets the DMG icons for macOS.
-func compileAndSetMacOSDMGIcons(dmgOutputPath, appName string) error {
+// compileSetIconSwiftScript compiles the seticon.swift script for macOS DMG.
+func compileSetIconSwiftScript() error {
 	fmt.Println("Compiling seticon.swift for macOS DMG")
 	if err := sh.Run("swiftc", "./build/darwin/seticon.swift"); err != nil {
 		return fmt.Errorf("error compiling seticon for macOS DMG: %v", err)
 	}
 
+	// Ensure the compiled script is executable
 	if err := sh.Run("chmod", "+x", "./seticon"); err != nil {
-		return fmt.Errorf("error setting permissions on seticon for macOS DMG: %v", err)
+		return fmt.Errorf("error setting permissions on seticon: %v", err)
 	}
 
+	return nil
+}
+
+// setDMGIcons runs the compiled seticon.swift script to set the icon for the DMG.
+func setDMGIcons(iconFilePath, dmgOutputPath string) error {
 	fmt.Println("Setting icons for macOS DMG")
-	if err := sh.RunV("./seticon", fmt.Sprintf("./build/bin/%s.app/Contents/Resources/iconfile.icns", appName), dmgOutputPath); err != nil {
+	if err := sh.RunV("./seticon", iconFilePath, dmgOutputPath); err != nil {
 		return fmt.Errorf("error setting icons for macOS DMG: %v", err)
 	}
 
@@ -116,46 +148,16 @@ func notarizeMacOSDMG(dmgPath string) error {
 	password := os.Getenv("AC_PASSWORD")
 	teamID := os.Getenv("AC_TEAM_ID")
 
-	// Error if notarization credentials are missing
 	if appleID == "" || password == "" || teamID == "" {
 		return fmt.Errorf("missing required environment variables: AC_APPLE_ID, AC_PASSWORD, or AC_TEAM_ID")
 	}
 
-	// Submit the DMG for notarization and wait for completion
 	fmt.Println("Submitting DMG for macOS notarization")
-	return sh.RunV("xcrun", "notarytool", "submit", dmgPath,
-		"--wait",
-		"--apple-id", appleID,
-		"--password", password,
-		"--team-id", teamID)
+	return sh.RunV("xcrun", "notarytool", "submit", dmgPath, "--wait", "--apple-id", appleID, "--password", password, "--team-id", teamID)
 }
 
 // stapleMacOSNotarization staples the notarization ticket to the macOS DMG.
 func stapleMacOSNotarization(dmgPath string) error {
 	fmt.Println("Stapling notarization ticket to macOS DMG")
 	return sh.RunV("xcrun", "stapler", "staple", dmgPath)
-}
-
-// importMacOSCodeSigningCertificates imports the code signing certificates into the keychain.
-func importMacOSCodeSigningCertificates(certBase64, password string) error {
-	if certBase64 == "" || password == "" {
-		return fmt.Errorf("missing required environment variables for code-signing")
-	}
-
-	certBytes, err := base64.StdEncoding.DecodeString(certBase64)
-	if err != nil {
-		return fmt.Errorf("error decoding base64 certificate: %v", err)
-	}
-
-	certFilePath := "./cert.p12"
-	if err := os.WriteFile(certFilePath, certBytes, 0600); err != nil {
-		return fmt.Errorf("error writing certificate to file: %v", err)
-	}
-	defer os.Remove(certFilePath)
-
-	if err := sh.Run("security", "import", certFilePath, "-P", password, "-T", "/usr/bin/codesign"); err != nil {
-		return fmt.Errorf("error importing certificate: %v", err)
-	}
-
-	return nil
 }
